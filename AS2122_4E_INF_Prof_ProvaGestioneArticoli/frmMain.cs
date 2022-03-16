0db0045c66fb5cb2d1085d4d0f8d22e92763e432
Benzi Alessandro <scaramacai.benzi@gmail.com>
2022-03-16 10:16:18 +0100

    - (2) aggiungere visualizzazione articoli inseriti nella listbox

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
@@ -49,6 +49,11 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
                 case "Visualizza articoli":
                     // TODO: (2) aggiungere visualizzazione articoli inseriti nella listbox
                     // ... lstElenco.Items.Add(a.Visualizzati()); 
+                    lstElenco.Items.Clear();
+                    foreach (Articolo c in Articoli)
+                    {
+                        lstElenco.Items.Add(c.Visualizzati());
+                    }
                     break;
             }
         }
