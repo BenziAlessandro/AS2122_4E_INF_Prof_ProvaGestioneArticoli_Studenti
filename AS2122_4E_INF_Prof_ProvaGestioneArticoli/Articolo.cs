bf0f73da61990a760d43eedc99b2e946ee479ec4
Benzi Alessandro <scaramacai.benzi@gmail.com>
2022-03-16 09:42:59 +0100

    Commit delle ore 09:42

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -10,13 +10,21 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
     {
         // TODO: (8) aggiungere attributi privati di Articolo
         // ...
-
+        public int Codice { get; private set; }
+        public string Descrizione { get; private set; }
+        public string Unita_Misura { get; private set; }
+        public double Prezzo { get; private set; }
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
         // ...
+    
         public Articolo(int codice, string descrizione, string unitaMisura, double prezzo)
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
             // ...
+            Codice = codice;
+            Descrizione = descrizione;
+            Unita_Misura = unitaMisura;
+            Prezzo = prezzo;
         }
 
         public string Visualizzati()
